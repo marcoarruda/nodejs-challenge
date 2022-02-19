eba571d6d3a03f3b53543a601dda1546c23cf8d0
Marco Arruda <marco.nc.arruda@gmail.com>
2022-02-19 11:55:34 -0300

    implement landRover method

--- src/MarsExploration.js
+++ src/MarsExploration.js
@@ -11,7 +11,14 @@ class MarsExploration {
   }
 
   landRover(roverId, name, landingPosition) {
-
+    const [ positionX, positionY, orientation ] = landingPosition.split(' ')
+    return {
+      id: roverId,
+      name,
+      positionX: parseInt(positionX),
+      positionY: parseInt(positionY),
+      orientation: orientation
+    }
   }
 
   moveRover(roverId, commands) {
